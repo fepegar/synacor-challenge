febc017041a9da5e0ba0a9904677148441a407a4
Fernando <fepegar@gmail.com>
2017-12-29 00:23:00 +0000

    Add all instructions

--- synacor.py
+++ synacor.py
@@ -4,37 +4,48 @@ import struct
 from os.path import join, dirname
 from collections import deque, defaultdict
 
-def read_instructions_map(input_path):
-    with open(input_path) as f:
-        lines = f.readlines()
-    instructions_lines = lines[35::2]
-    instructions = {}
-    for i, line in enumerate(instructions_lines):
-        name = line.split(':')[0]
-        argc = len(line.split()) - 2
-        instructions[i] = name, argc
-    return instructions
+MIN_REGISTER = 32768
+MAX_VALID = 32775
+HEX_15BIT = 0x7FFF
+
 
 
 class VirtualMachine:
     def __init__(self, arch_spec_path, binary_path):
-        self.instructions_map = read_instructions_map(arch_spec_path)
+        self.instructions_map = self.read_instructions_map(arch_spec_path)
         self.stack = deque()
         self.registers = defaultdict(int)
         self.binary_file = None
         self.current_address = 0
         self.memory = self.read_program(binary_path)
 
+    # def initialize_memory(self):
+    #     memory = {}
+    #     for n in range(MIN_REGISTER):
+    #         memory[n] = 0
+    #     return memory
+
+    def read_instructions_map(self, input_path):
+        with open(input_path) as f:
+            lines = f.readlines()
+        instructions_lines = lines[35::2]
+        instructions = {}
+        for i, line in enumerate(instructions_lines):
+            name = line.split(':')[0]
+            argc = len(line.split()) - 2
+            instructions[i] = name, argc
+        return instructions
+
     def read_program(self, binary_path):
         memory = {}
         with open(binary_path, 'rb') as f:
-            # while True:
-            for _ in range(350):
+            while True:
+            # for _ in range(350):
                 next_instruction = self.read_next_instruction(f)
+                print(next_instruction)
                 if next_instruction is None:
-                    return memory
+                    break
                 if next_instruction is not None:
-                    # print(next_instruction)
                     address, name, argv = next_instruction
                     memory[address] = name, argv
         return memory
@@ -58,22 +69,38 @@ class VirtualMachine:
     def read_words(self, file, n=1):
         return [self.read_word(file) for _ in range(n)]
 
-    def translate(self, n):
-        if n <  32768:
-            return n
-        elif n >= 32768:
-            return self.registers[n - 32768]
-        else:
+    def parse(self, n):
+        if n < 0 or n > MAX_VALID:
             raise ValueError('Invalid number:', n)
+        elif n >= MIN_REGISTER:
+            return self.registers[n - MIN_REGISTER]
+        else:
+            return n
 
+    def fix_name(self, name):
+        if name in ('and', 'or', 'not', 'in'):
+            name += '_'
+        return name
+
+    def run_next_instruction(self):
+        name, argv = self.memory[self.current_address]
+        argc = len(argv)
+        self.current_address += 1 + argc
+        getattr(self, self.fix_name(name))(*argv)
+
+    def run(self):
+        while True:
+            self.run_next_instruction()
+
+    ### INSTRUCTIONS ###
     def halt(self):
         sys.exit(0)
 
     def set(self, a, b):
-        self.registers[a] = self.registers[b]
+        self.registers[a] = self.parse(b)
 
     def push(self, a):
-        self.stack.append(a)
+        self.stack.append(self.parse(a))
 
     def pop(self, a):
         try:
@@ -83,62 +110,77 @@ class VirtualMachine:
             sys.exit(1)
 
     def eq(self, a, b, c):
-        self.registers[a] = 1 if self.registers[b] == self.registers[c] else 0
+        if self.parse(b) == self.parse(c):
+            self.set(a, 1)
+        else:
+            self.set(a, 0)
 
     def gt(self, a, b, c):
-        self.registers[a] = 1 if self.registers[b] > self.registers[c] else 0
+        if self.parse(b) > self.parse(c):
+            self.set(a, 1)
+        else:
+            self.set(a, 0)
 
     def jmp(self, a):
-        self.current_address = self.regis
+        self.current_address = self.parse(a)
+
+    def jt(self, a, b):
+        if self.parse(a) != 0:
+            self.jmp(b)
+
+    def jf(self, a, b):
+        if self.parse(a) == 0:
+            self.jmp(b)
+
+    def add(self, a, b, c):
+        result = (self.parse(b) + self.parse(c)) % MIN_REGISTER
+        self.set(a, result)
+
+    def mult(self, a, b, c):
+        result = (self.parse(b) * self.parse(c)) % MIN_REGISTER
+        self.set(a, result)
+
+    def mod(self, a, b, c):
+        result = self.parse(b) % self.parse(c)
+        self.set(a, result)
+
+    def and_(self, a, b, c):
+        result = self.parse(b) & self.parse(c)
+        self.set(a, result)
+
+    def or_(self, a, b, c):
+        result = self.parse(b) | self.parse(c)
+        self.set(a, result)
+
+    def not_(self, a, b):
+        result = (~self.parse(b)) & HEX_15BIT
+        self.set(a, result)
+
+    def rmem(self, a, b):
+        self.set(a, self.memory[self.parse(b)])
+
+    def wmem(self, a, b):
+        self.set(a, self.parse(b))
+
+    def call(self, a):
+        self.push(self.current_address)
+        self.jmp(self.parse(a))
+
+    def ret(self):
+        try:
+            self.jmp(self.stack.pop())
+        except IndexError:
+            self.halt()
 
     def out(self, a):
         print(chr(a), end='')
 
+    def in_(self, a):
+        self.set(ord(a), input())
+
     def noop(self):
         pass
 
-    def run_next_instruction(self):
-        name, argv = self.memory[self.current_address]
-        argc = len(argv)
-        self.current_address += 1 + argc
-        # print('Running', name, argv)
-        getattr(self, name)(*argv)
-
-
-        # argc = len(argv)
-        #
-        # if argc > 0:
-        #     a = argv[0]
-        # if argc > 1:
-        #     b = argv[1]
-        # if argc > 2:
-        #     c = argv[2]
-        #
-        # if name == 'halt':
-        #     return False
-        # elif name == 'set':
-        #     self.registers[a] = b
-        # elif name == 'push':
-        #     self.stack.append(a)
-        # elif name == 'pop':
-        #     try:
-        #         self.registers[a] = self.stack.pop()
-        #     except IndexError:
-        #         print('The stack is empty!')
-        #         return False
-        # elif name == 'eq':
-        #     a = 1 if b == c else 0
-        # elif name == 'gt':
-        #     a = 1 if b == c else 0
-        # elif name == 'jmp':
-        #     self.current_address = a
-        # else:
-        #     pass # TODO
-        # return True
-
-    def run(self):
-        while True:
-            self.run_next_instruction()
 
 
 def main():
